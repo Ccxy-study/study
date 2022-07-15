f200217df80046777bef07ea2ccacbcf4925f210
Ccxy-study <ykz8.24@outlook.com>
2022-07-15 23:48:56 +0800

    init

---
+++ removeElement.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int removeElement(int *nums, int size, int val);
+
+int main()
+{
+    int nums[6] = {0, 2, 3, 7, 2, 5};
+    int size = removeElement(nums, 6, 2);
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d", nums[i]);
+    }
+}
+
+int removeElement(int *nums, int numsSize, int val)
+{
+    for (int i = 0; i < numsSize; i++)
+    {
+        if (nums[i] == val)
+        {
+            for (int j = i; j < numsSize - i - 1; j++)
+            {
+                nums[j] = nums[j + 1];
+            }
+            numsSize--;
+            i--;
+        }
+    }
+    return numsSize;
+}
